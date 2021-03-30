bd0c7725bc1b17496b99fb2b6fa359206bea0c04
Zahir Begulić <46743547+Zahir8@users.noreply.github.com>
2021-03-30 10:24:56 +0200

    updated Box.js

--- ex04/src/Box.js
+++ ex04/src/Box.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Box extends Component {
     render() {
         return (
-            <div>This is a Box Component</div>
+            <div>This is a Box Component!</div>
         )
     }
 }
